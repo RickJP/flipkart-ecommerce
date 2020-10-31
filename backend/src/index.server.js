f978844353b479a12a45016d0d8daacce0cd60d0
rickjp <3741326+RickJP@users.noreply.github.com>
2020-10-31 22:06:42 +0900

    ADD: 2 - Signup route, user model and controllerp

--- backend/src/index.server.js
+++ backend/src/index.server.js
@@ -1,22 +1,32 @@
 const express = require('express');
-require('dotenv');
+require('dotenv').config();
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const port = process.env.PORT || 3000;
+const userRoutes = require('./routes/user');
 
-app.use(express.json());
+const env = process.env;
+const port = env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'hi from server',
+mongoose
+  .connect(
+    `
+    mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.gtgv3.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority
+`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    },
+  )
+  .then(() => {
+    console.log('Database connected');
   });
-});
 
-app.post('/data', (req, res) => {
-  res.status(200).json({
-    message: req.body,
-  });
-});
+//app.use(express.json());
+app.use(bodyParser.json());
+app.use('/api', userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
