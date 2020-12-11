e26448e294027342a432668599a0d78d427dac02
rickjp <3741326+RickJP@users.noreply.github.com>
2020-12-11 11:00:19 +0900

    ADD: 37 - Place Order

--- frontend/src/containers/CheckoutPage/index.js
+++ frontend/src/containers/CheckoutPage/index.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAddress } from '../../actions';
 import Layout from '../../components/Layout';
-import { MaterialButton } from '../../components/MaterialUI';
+import { MaterialButton, MaterialInput } from '../../components/MaterialUI';
 import AddressForm from './AddressForm';
 import './style.css';
 
 const CheckoutStep = (props) => {
   return (
     <div className='checkoutStep'>
-      <div className={`checkoutHeader ${props?.active && 'active'}`}>
+      <div
+        onClick={props.onClick}
+        className={`checkoutHeader ${props?.active && 'active'}`}>
         <div>
           <span className='stepNumber'>{props.stepNumber}</span>
           <span className='stepTitle'>{props.title}</span>
@@ -23,13 +25,38 @@ const CheckoutStep = (props) => {
 const CheckoutPage = (props) => {
   const user = useSelector((state) => state.user);
   const auth = useSelector((state) => state.auth);
+  const [newAddress, setNewAddress] = useState(false);
+  const [address, setAddress] = useState([]);
+  const [confirmAddress, setConfirmAddress] = useState(false);
+  const [selectedAddress, setSelectedAddress] = useState(null);
   const dispatch = useDispatch();
 
   const onAddressSubmit = () => {};
 
+  const selectAddress = (addr) => {
+    const updatedAddress = address.map((adr) =>
+      adr._id === addr._id ? { ...adr, selected: true } : { ...addr },
+    );
+    setAddress(updatedAddress);
+  };
+
+  const confirmDeliveryAddress = (addr) => {
+    setConfirmAddress(true);
+    setSelectedAddress(addr);
+  };
+
   useEffect(() => {
-    dispatch(getAddress());
-  }, []);
+    auth.authenticate && dispatch(getAddress());
+  }, [auth.authenticate, dispatch]);
+
+  useEffect(() => {
+    const address = user.address.map((adr) => ({
+      ...adr,
+      selected: false,
+      edit: false,
+    }));
+    setAddress(address);
+  }, [user.address]);
 
   return (
     <Layout>
@@ -41,51 +68,74 @@ const CheckoutPage = (props) => {
             title={'LOGIN'}
             active={!auth.authenticate}
             body={
-              <div className='loggedInId'>
-                <span style={{ fontWeight: 500 }}>{auth.user.fullName}</span>
-                <span style={{ margin: '0 5px' }}>{auth.user.email}</span>
-              </div>
+              auth.authenticate ? (
+                <div className='loggedInId'>
+                  <span style={{ fontWeight: 500 }}>{auth.user.fullName}</span>
+                  <span style={{ margin: '0 5px' }}>{auth.user.email}</span>
+                </div>
+              ) : (
+                <div>
+                  <MaterialInput label='Email' />
+                </div>
+              )
             }
           />
           <CheckoutStep
             stepNumber={'2'}
             title={'DELIVERY ADDRESS'}
-            active={true}
+            active={!confirmAddress}
             body={
               <>
-                {user.address?.map((adr) => (
-                  <div className='flexRow addressContainer'>
-                    <div>
-                      <input name='address' type='radio' />
-                    </div>
-                    <div className='flexRow sb addressInfo'>
-                      <div>
+                {confirmAddress
+                  ? JSON.stringify(selectedAddress)
+                  : address?.map((adr) => (
+                      <div key={adr._id} className='flexRow addressContainer'>
                         <div>
-                          <span>{adr.name}</span>
-                          <span>{adr.addressType}</span>
-                          <span>{adr.mobileNumber}</span>
+                          <input
+                            name='address'
+                            type='radio'
+                            onClick={() => selectAddress(adr)}
+                          />
                         </div>
-                        <div>{adr.address}</div>
-                        <div>{adr.locality}</div>
-                        <div>{adr.cityDistrictTown}</div>
-                        <div>{adr.state}</div>
-                        <div>{adr.landmark}</div>
-                        <MaterialButton
-                          title='DELIVERY HERE'
-                          style={{ width: '250px' }}
-                        />
+                        <div className='flexRow sb addressInfo'>
+                          <div>
+                            <div>
+                              <span>{adr.name}</span>
+                              <span>{adr.addressType}</span>
+                              <span>{adr.mobileNumber}</span>
+                            </div>
+                            <div>{adr.address}</div>
+                            <div>{adr.locality}</div>
+                            <div>{adr.cityDistrictTown}</div>
+                            <div>{adr.state}</div>
+                            <div>{adr.landmark}</div>
+                            {adr.selected && (
+                              <MaterialButton
+                                title='DELIVERY HERE'
+                                style={{ width: '250px' }}
+                                onClick={() => confirmDeliveryAddress(adr)}
+                              />
+                            )}
+                          </div>
+                          {adr.selected && <div>edit</div>}
+                        </div>
                       </div>
-                      <div>edit</div>
-                    </div>
-                  </div>
-                ))}
+                    ))}
               </>
             }
           />
 
           {/* Address Form */}
-
-          <AddressForm onSubmitForm={onAddressSubmit} onCancel={() => {}} />
+          {confirmAddress ? null : newAddress ? (
+            <AddressForm onSubmitForm={onAddressSubmit} onCancel={() => {}} />
+          ) : (
+            <CheckoutStep
+              stepNumber={'+'}
+              title={'ADD NEW ADDRESS'}
+              active={false}
+              onClick={() => setNewAddress(true)}
+            />
+          )}
 
           <CheckoutStep stepNumber={'3'} title={'ORDER SUMMARY'} />
           <CheckoutStep stepNumber={'4'} title={'PAYMENT OPTIONS'} />
